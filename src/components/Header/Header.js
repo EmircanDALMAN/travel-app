27d586ea2094ebb2cbace0ac36d7c977b61c7e5e
Emircan Dalman <75789956+EmircanDALMAN@users.noreply.github.com>
2022-01-16 23:49:59 +0300

    project finished!

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+
+import useStyles from './styles.js';
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
